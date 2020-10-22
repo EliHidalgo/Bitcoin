899802cbf13c72837b014c6e7fe534e31fe897b7
EliHidalgo <hidalgo_eliana@hotmail.com>
2020-10-22 12:25:21 -0300

    refactoring the code

--- src/components/BitcoinRate.jsx
+++ src/components/BitcoinRate.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/BitcoinRate.css';
 import axios from 'axios';
 import BpiChart from './BpiChart';
@@ -7,31 +7,27 @@ const BitcoinRate = () => {
 
     const [ rate, setRate] = useState();
 
-const gettingRate = useCallback(() => {
+const gettingRate = () => {
     axios.get('https://api.coindesk.com/v1/bpi/currentprice/USD.json')
         .then(result => {
-            //console.log(result.data.bpi.USD.rate);
-            let rateResult = result.data.bpi.USD.rate;
-            let rateSlice1 = rateResult.slice(0,2);
-            let rateSlice2 = rateResult.slice(3)
-            let newRate = rateSlice1.concat(rateSlice2);
-            //console.log(newRate, 'newRate', typeof newRate)
-            let floatRate = parseFloat(newRate);
-            //console.log(floatRate, 'floatRate', typeof floatRate);
-            let final = Math.round(floatRate);
+            //console.log(result.data.bpi.USD.rate_float);
+            let rateResult = result.data.bpi.USD.rate_float;
+            let final = Math.round(rateResult);
             setRate(new Intl.NumberFormat('es-CL').format(final))
         })
         .catch( err => {
             console.log(err);
-        }); } , []);
+        });
+    }
 
      
     useEffect(() => {
+        gettingRate();
         setInterval(
             () => gettingRate(),
             1000
             );        
-    }, [gettingRate])
+    })
 
  
      
